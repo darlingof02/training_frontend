6e750397dd8c7104c21b677b3f805a8993d9fad4
Yizhuang Peng <yizhuang.peng@kaseya.com>
2023-03-09 15:55:46 -0500

    all

--- src/main/java/com/kaseya/controllers/JwtAuthenticationController.java
+++ src/main/java/com/kaseya/controllers/JwtAuthenticationController.java
@@ -35,7 +35,7 @@ public class JwtAuthenticationController {
         authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUsername());
-
+        System.out.println("Authentication is working");
         final String token = jwtTokenUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new JwtResponse(token));
